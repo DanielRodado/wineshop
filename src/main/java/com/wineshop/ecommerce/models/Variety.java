a94a1a4e46206197eb4ca89cd53637364add7d52
DanielRodado <d4nielrodado@gmail.com>
2023-11-22 13:19:21 -0500

    Se agregaron las constantes en el Enum Variety

--- src/main/java/com/wineshop/ecommerce/models/Variety.java
+++ src/main/java/com/wineshop/ecommerce/models/Variety.java
@@ -2,7 +2,29 @@ package com.wineshop.ecommerce.models;
 
 public enum Variety {
 
+    BRUT,
+    BRUT_NATURE,
+    CABERNET_SAUVIGNON,
+    CARIGNAN,
     CARMENERE,
-    MALBEC
+    CAVA,
+    CHAMPAGNE,
+    CHARDONNAY,
+    COASTAL_BRUT,
+    ENSAMBLAJE,
+    EXTRA_BRUT,
+    MALBEC,
+    MERLOT,
+    MOSCATEL,
+    PAIS,
+    PETIT,
+    PETIT_VERDOT,
+    PINOT_NOIR,
+    RIESLING,
+    ROSE,
+    SAUVIGNON_BLANC,
+    SYRAH,
+    VIOGNIER,
+    VERDEJO,
 
 }
